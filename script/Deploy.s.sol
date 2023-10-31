82e81821c0978cb790930ef2bb56448c0963fc9e
Ben DiFrancesco <ben@scopelift.co>
2023-10-30 23:02:14 -0400

    Setup remappings and governor files

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,16 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 // slither-disable-start reentrancy-benign
-
-pragma solidity 0.8.20;
+pragma solidity 0.8.22;
 
 import {Script} from "forge-std/Script.sol";
-import {Counter} from "src/Counter.sol";
 
 contract Deploy is Script {
-  Counter counter;
 
-  function run() public {
-    vm.broadcast();
-    counter = new Counter();
-  }
+  function run() public { }
 }
---
+++ test/GuineaPigGovernor.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+contract GuineaPigGovernorTest is Test {
+  function setUp() public {
+  }
+}
+
+contract Deployment is GuineaPigGovernorTest {
+
+}
