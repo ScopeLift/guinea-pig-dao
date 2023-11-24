20e2d160623833bf67ba855dfa5c4f408d842335
Ben DiFrancesco <ben@scopelift.co>
2023-11-24 14:18:09 -0500

    Update flexible-voting to version v1.1.3
    This version of Flexible Voting updates the underlying openzeppelin-contracts
    dependency to version v4.9.3

--- test/GuineaPigGovernor.t.sol
+++ test/GuineaPigGovernor.t.sol
@@ -3,20 +3,35 @@ pragma solidity 0.8.22;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {GuineaPigGovernor, IVotes, TimelockController} from "src/GuineaPigGovernor.sol";
+import {GuineaPigToken} from "src/GuineaPigToken.sol";
 
 contract GuineaPigGovernorTest is Test {
   GuineaPigGovernor governor;
 
-  IVotes token = IVotes(address(0x01b));
-  TimelockController timelock = TimelockController(payable(address(0x01c)));
+  IVotes token;
+  TimelockController timelock;
 
   uint256 INITIAL_VOTING_DELAY = 50;
   uint256 INITIAL_VOTING_PERIOD = 7200;
   uint256 INITIAL_PROPOSAL_THRESHOLD = 100_000e18;
 
   function setUp() public {
-    governor =
-    new GuineaPigGovernor(token, INITIAL_VOTING_DELAY, INITIAL_VOTING_PERIOD, INITIAL_PROPOSAL_THRESHOLD, timelock);
+    timelock = new TimelockController(
+      100,
+       new address[](0),
+       new address[](0),
+      address(this)
+    );
+
+    token = new GuineaPigToken(address(timelock), address(0xcafe), 100e18);
+
+    governor = new GuineaPigGovernor(
+      token,
+      INITIAL_VOTING_DELAY,
+      INITIAL_VOTING_PERIOD,
+      INITIAL_PROPOSAL_THRESHOLD,
+      timelock
+    );
   }
 }
 
