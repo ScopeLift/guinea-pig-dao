009fdc73ace65f6956669a10b453119a8319a654
Ben DiFrancesco <ben@scopelift.co>
2024-01-01 16:04:04 -0500

    Configure and deploy Guinea Pig Dao contracts

--- script/DeployLaunchConstants.sol
+++ script/DeployLaunchConstants.sol
@@ -7,6 +7,6 @@ contract DeployLaunchConstants {
   uint256 INITIAL_VOTING_PERIOD = 7200; // 1 day given 12 second blocks
   uint256 INITIAL_PROPOSAL_THRESHOLD = 24_000e18; // 2% of total supply
   uint256 TIMELOCK_MIN_DELAY = 300; // 300 sec == 5 minutes
-  address INITIAL_MINT_RECEIVER = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; // default anvil
+  address INITIAL_MINT_RECEIVER = 0x5C04E7808455ee0e22c2773328C151d0DD79dC62;
   uint256 INITIAL_MINT_AMOUNT = 1_200_000e18;
 }
